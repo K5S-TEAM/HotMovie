d6f63b291562216b85cd47ec6f07424bd16d0353
emrhssla <emrhssla@gmail.com>
2022-04-04 14:48:34 +0900

    기본 도메인 개발
    - Member 모델 구축
    - Movie 모델 구축
    - Review 모델 구축
    close #6

---
+++ chan/src/main/java/k5s/reviewdevelop/domain/Member.java
@@ -0,0 +1,39 @@
+package k5s.reviewdevelop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviews = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "bookmark",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private List<Movie> movies = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+        movie.addMember(this);
+    }
+
+}
