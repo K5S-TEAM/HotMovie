fc89387be3c9188c4a053bce2dedfff5497ba980
ChanYoung <emrhssla@gmail.com>
2022-04-07 14:11:51 +0900

    [개발 #24]로그인에 따른 Review 쓰기 버튼
    #24  참조

--- chan/src/main/java/k5s/reviewdevelop/controller/ReviewController.java
+++ chan/src/main/java/k5s/reviewdevelop/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package k5s.reviewdevelop.controller;
 
 
+import k5s.reviewdevelop.domain.Member;
 import k5s.reviewdevelop.domain.Movie;
 import k5s.reviewdevelop.domain.Review;
 import k5s.reviewdevelop.repository.ReviewRepository;
@@ -12,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
@@ -33,7 +35,26 @@ public class ReviewController {
             return "movies/reviews/error";
         }
         model.addAttribute("movieName", movie.getName());
+        model.addAttribute("movieId", movie.getId());
         model.addAttribute("reviews", movie.getReviews());
         return "movies/reviews/reviewList";
+    }
+
+    @GetMapping("/{movieId}/reviews/new")
+    public String write(@SessionAttribute(name = "loginMemberTEST", required = false) Member loginMember, @PathVariable("movieId") Long movieId, Model model){
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "redirect:/loginPage";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "movies/reviews/new";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
     }
 }
